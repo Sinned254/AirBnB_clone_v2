cda3c4fbdfa9049d517cca91290023db8a9199c4
Denis Kosgei <dennis1kosgei@gmail.com>
2023-11-22 01:46:58 -0800

    add files

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    # One user to many places
+    places = relationship('Place', cascade='all, delete', backref='user')
+
+    # One user can create many reviews
+    reviews = relationship('Review', cascade='all, delete', backref='user')
